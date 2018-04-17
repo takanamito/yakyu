ada145780716ae4ceb0ccac4017466ba1baab02f
takanamito <takanamito0928@gmail.com>
2018-04-17 22:52:45 +0900

    Add GamesWide fields

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,17 +1,7 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name 'Query'
 
-  field :games_wide, !Types::GamesWideType do
-    argument :hitter_first_name, !types.String
-    argument :hitter_last_name, !types.String
-
-    resolve ->(obj, args, ctx) {
-      GamesWide.find_by(
-        hitterFirstName: args[:hitter_first_name],
-        hitterLastName: args[:hitter_last_name]
-      )
-    }
-  end
+  field :games_wide, !Types::GamesWideType, field: Fields::GamesWide.build
 
   field :pitch_type_count, !Types::Pitch::CountByPitchType do
     argument :pitcher_first_name, !types.String
