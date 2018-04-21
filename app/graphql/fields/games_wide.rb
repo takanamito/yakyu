d356514cb31e6a06b36f3040e1dfef89144acef2
takanamito <takanamito0928@gmail.com>
2018-04-21 12:43:15 +0900

    Add parameter to games_wide

--- app/graphql/fields/games_wide.rb
+++ app/graphql/fields/games_wide.rb
@@ -2,15 +2,15 @@ module Fields
   class GamesWide
     def self.build
       GraphQL::Field.define do
-        type GamesWide
-        argument :hitter_first_name, !types.String
-        argument :hitter_last_name, !types.String
+        argument :pitcher_first_name, !types.String
+        argument :pitcher_last_name, !types.String
+        argument :hit_allowed, types.Boolean, default_value: false
 
         resolve ->(obj, args, ctx) {
-          ::GamesWide.find_by(
-            hitterFirstName: args[:hitter_first_name],
-            hitterLastName: args[:hitter_last_name]
-          )
+          searcher = PitchSearcher.new(first_name: args[:pitcher_first_name], last_name: args[:pitcher_last_name])
+
+          return searcher.hits if args[:hit_allowed]
+          searcher.all
         }
       end
     end
